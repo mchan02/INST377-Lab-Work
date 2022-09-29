f70f2349184556755a28a25c0c9423522a7cf0ed
mchan02 <90062401+mchan02@users.noreply.github.com>
2022-09-28 22:02:18 -0400

    start/pause

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score');
   const StartButton = document.querySelector('#start-button');
   const width = 10;
+  let nextRandom = 0;
+  let timerId;
 
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
@@ -47,28 +49,21 @@ document.addEventListener('DOMContentLoaded', () => {
   let random = Math.floor(Math.random()*theTetrominoes.length);
   let current = theTetrominoes[random][currentRotation];
 
+  // draws the tetrimino
   function draw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino')
     });
   }
   
+  // undraws/erases the tetrimino
   function undraw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.remove('tetromino')
   });
   }
 
-  function freeze() {
-    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-      current.forEach(index => squares[currentPosition + index].classList.add('taken'));
-      random = Math.floor(Math.random() * theTetrominoes.length);
-      current = theTetrominoes[random][currentRotation];
-      currentPosition = 4;
-      draw();
-    }
-  }
-
+  // allows the tetriminoes to fall down
   function moveDown() {
     undraw();
     currentPosition += width;
@@ -76,8 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
     freeze();
   }
 
-  timerId = setInterval(moveDown, 1000);
-
+  // moves the tetriminoes to the left when the left arrow key is pressed
   function moveLeft() {
     undraw();
     const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
@@ -88,6 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
     draw();
   }
 
+  // moves the tetriminoes to the right when the right arrow key is pressed
   function moveRight() {
     undraw();
     const isAtRightEdge = current.some(index => (currentPosition + index) % width === (width - 1));
@@ -98,6 +93,7 @@ document.addEventListener('DOMContentLoaded', () => {
     draw();
   }
 
+  // rotates the tetrominoes when the up arrow key is pressed
   function rotate() {
     undraw();
     currentRotation++;
@@ -108,6 +104,7 @@ document.addEventListener('DOMContentLoaded', () => {
     draw();
   }
 
+  // matches the arrow key to the corresponding functions
   function control(key) {
     if (key.keyCode === 37) {
       moveLeft();
@@ -120,5 +117,52 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  // shows upcoming tetriminoes in mini grid display
+  const displaySquares = document.querySelectorAll('.mini-grid div');
+  const displayWidth = 4;
+  let displayIndex = 0;
+
+  const upNextTetrominoes = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2], // l
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], // z
+    [1, displayWidth, displayWidth + 1, displayWidth + 2], // t
+    [0, 1, displayWidth, displayWidth + 1], // o
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] // i
+  ];
+
+  function displayShape() {
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+    });
+    upNextTetrominoes[nextRandom].forEach(index => {
+      displaySquares[displayIndex + index].classList.add('tetromino')
+    });
+  }
+
+  // stops the tetriminoes from falling off of the screen
+  function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+      random = nextRandom; 
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+      displayShape();
+    }
+  }
+
   document.addEventListener('keyup', control);
+
+  StartButton.addEventListener('click', () => {
+    if (timerId) {
+      clearInterval(timerId);
+      timerId = null;
+    } else {
+      draw();
+      timerId = setInterval(moveDown, 1000);
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+      displayShape();
+    }
+  })
 });
